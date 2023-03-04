4c25829b2e665c9364bdcf33e0410a03ce7e5877
0xkkl <kaydan@leozc.com>
2023-03-04 12:31:22 -0800

    fixed some commands

--- bot.py
+++ bot.py
@@ -170,7 +170,7 @@ async def status_task() -> None:
     """
     Setup the game status task of the bot.
     """
-    statuses = ["with you!", "with Krypton!", "with humans!"]
+    statuses = ["With the blockchain", "With the Ethereum blockchain"]
     await bot.change_presence(activity=discord.Game(random.choice(statuses)))
 
 
