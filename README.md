5a9065e4961308bb208b973273870a6179ea9998
Leo Liang <leozc@leozc.com>
2023-03-04 12:26:18 -0800

    adding features

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Bot will reply
 0x11111 is a wallet address on network x y z, 
 it has metadata x y z
 ....
+
+
+```
+Good contract 
+0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48 eth
+
+Bad contract 
+0xe20747ac34e5e481c6cb6b687512258e5b534f6e bsc (56)
+see https://cryptogems.tips/scanner?address=0xe20747ac34e5e481c6cb6b687512258e5b534f6e&blockchain=bsc
--- tests/api_test.py
+++ tests/api_test.py
@@ -1,6 +1,7 @@
 # Write unit tests for the API here for goplusapicaller
 
 import unittest
+import json
 
 from helpers import goplusapicaller
 class TestStringMethods(unittest.TestCase):
@@ -14,45 +15,23 @@ class TestStringMethods(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(goplusapicaller.hello(), 'hello')
 
-    # def test_getContractScan(self):
-    #     self.assertEqual(
-    #         goplusapicaller.getContractScan(
-    #             1, "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"),
-    #         "Wallet: 0x1234")
+    def test_getContractScan_good(self):
+        # good address
+        json_string = json.dumps(goplusapicaller.getContractScan(
+            1, "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"), indent=2)
+        print("good address")
 
-    def langMatch(self):
-        import nltk
+        #print(json_string)
+        self.assertEqual(goplusapicaller.hello(), 'hello')
 
-        from nltk.util import ngrams
+    def test_getContractScan_bad(self):
+        # bad address
+        addr ="0xe20747ac34e5e481c6cb6b687512258e5b534f6e"
+        ret = goplusapicaller.getContractScan(56, addr)
+        print("bad address")
 
-        # Define the list of NFT names
-        nft_names = [
-            "CryptoKitties", "Axie Infinity", "Bored Ape Yacht Club",
-            "Pudgy Penguins", "Meebits", "Art Blocks", "World of Women",
-            "Mutant Ape Yacht Club", "Cool Cats", "Loot"
-        ]
-
-        # Define the input string to search for fuzzy matches
-        input_string = "Axie Infiniti"
-
-        # Define the length of the ngrams to use for matching
-        ngram_length = 3
-
-        # Define a function to calculate the Jaccard similarity between two sets
-        def jaccard_similarity(s1, s2):
-            intersection = len(s1.intersection(s2))
-            union = len(s1.union(s2))
-            return intersection / union
-
-        # Create an ngram set from the input string
-        input_ngrams = set(ngrams(input_string.lower(), ngram_length))
-
-        # Loop over the NFT names and calculate the Jaccard similarity of each name to the input string
-        for name in nft_names:
-            name_ngrams = set(ngrams(name.lower(), ngram_length))
-            similarity = jaccard_similarity(input_ngrams, name_ngrams)
-            print(f"{name}: {similarity}")
-        
+        print(goplusapicaller.extractSafeyVector(addr, ret))
+        # print(json_string)
         self.assertEqual(goplusapicaller.hello(), 'hello')
 
 
